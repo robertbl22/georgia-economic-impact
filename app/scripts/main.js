92993cd026cd2116ced7fd63df0c8de8529dc8c7
robertbl22 <robertbl22@gmail.com>
2013-04-29 21:01:04 -0400

    Added StateMapControl region mouseover highlight, click to display region, and map load/resize animation.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -11,7 +11,7 @@ require.config({
 require(['routes/router'], function(router) {"use strict";
 
     $("#GPALogoBanner").delay(50).slideDown();
-
+    
     //infuser.defaults.templateSuffix = ".tmpl.html";
     infuser.defaults.templateUrl = "/views";
 
--- app/scripts/viewmodels/ui-controls/statemapcontrol.js
+++ app/scripts/viewmodels/ui-controls/statemapcontrol.js
@@ -8,8 +8,27 @@ define([], function() {"use strict";
         /*************************************/
         /* Private Properties */
 
-        var $stateMapControl = $("#StateMapControl");
-        $stateMapControl.addClass("small").fadeIn().removeClass("small");
+        self.StateMapControlID = "#StateMapControl";
+        self.StateMapImageID = "#StateMapImg";
+        var resizeTimer = null;
+        self.MapsterOptions = {
+            singleSelect : true,
+            fillColor : '00284C',
+            stroke : true,
+            strokeColor : 'FFFFFF',
+            strokeWidth : 2,
+            clickNavigate : true,
+            showToolTip : true
+        };
+
+        /*************************************/
+        /* Event Handlers */
+
+        $(window).resize(function() {
+            if (resizeTimer)
+                clearTimeout(resizeTimer);
+            resizeTimer = setTimeout(playResizeAnimation, 50);
+        });
 
         /*************************************/
         /* Public Methods */
@@ -24,12 +43,50 @@ define([], function() {"use strict";
         /*************************************/
         /* Private Methods */
 
+        function playResizeAnimation() {
+            console.log("[" + self.ViewType + "] .playResizeAnimation() running");
+            var map = $(self.StateMapImageID);
+            map.mapster('unbind');
+            map.animate("width : ''" + getMapWidth() + "px'", 500, function() {
+                $(this).mapster(self.MapsterOptions);
+            });
+        };
+
+        function playLoadAnimation(img) {
+            //$(this).addClass("small").fadeIn().removeClass('small');
+            //$(this).mapster(self.MapsterOptions);
+            var smallWidth = Math.floor(getMapWidth() * .9);
+            //$(this).width(smallWidth);
+            img.width(smallWidth);
+            img.animate({
+                width : getMapWidth()
+            }, 500, function() {
+                img.mapster(self.MapsterOptions);
+            });
+        };
+
+        function getMapWidth() {
+            // NOTE: original image width is 424px
+            var colWidth = $(".span4").width();
+            var newWidth = Math.floor(colWidth * .9);
+            var maxWidth = 380;
+            if (newWidth > maxWidth)
+                newWidth = maxWidth;
+            return newWidth;
+        };
+
         self.RenderTemplate = function($eventTarget, $parentElement, tpl) {
             $parentElement.append(tpl);
-            var stateMapControl = $parentElement.find("#StateMapControl").get(0);
+            var stateMapControl = $parentElement.find(self.StateMapControlID).get(0);
             ko.applyBindings(self, stateMapControl);
             console.log("[" + self.ViewType + "] StateMapControl triggering ui_controls_loaded");
-            $eventTarget.trigger("ui_controls_loaded", "StateMapControl");
+
+            $(self.StateMapImageID).load(function() {
+                console.log("[" + self.ViewType + "] RenderTemplate() #StateMapImg loaded");
+                playLoadAnimation($(this));
+            });
+
+            $eventTarget.trigger("ui_controls_loaded", self.ViewType);
         };
 
         /* Public interface */
