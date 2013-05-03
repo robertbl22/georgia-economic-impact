262350e543f959b0bca52c65ca2ecef27acf3704
robertbl22 <robertbl22@gmail.com>
2013-05-03 12:21:58 -0400

    Commented out all console.log()

---
+++ dist/scripts/viewmodels/viewmodels.js
@@ -0,0 +1,14 @@
+define(["viewmodels/entities/entities", "viewmodels/layouts/layouts", "viewmodels/tabs/tabs", "viewmodels/ui-controls/usercontrols"], function(Entities, Layouts, Tabs, UserControls) {"use strict";
+
+    /************************/
+    /* Public interface */
+
+    return {
+        Entities : Entities,
+        Layouts : Layouts,
+        Tabs : Tabs,
+        UserControls : UserControls
+    };
+
+});
+
